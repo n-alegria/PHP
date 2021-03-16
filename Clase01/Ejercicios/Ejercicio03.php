3e5038aea4935aff6ac919b377565ac2f2b8115a
Lautaro <n.alegria@outlook.com>
2021-03-16 17:46:25 -0300

    Clase 1

---
+++ Clase01/Ejercicios/Ejercicio03.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+Aplicación Nº 3 (Sumar dos números II)
+Partiendo del ejercicio anterior, modificar la salida por pantalla para que se visualice el valor de
+la variable $x, el valor de la variable $y y el resultado final en líneas distintas (recordar que el
+salto de línea en HTML es la etiqueta <br/>).
+*/
+
+$x = -3;
+$y = 15;
+
+$resuldato = $x + $y;
+
+echo 'El valor de X es: ' . $x . '<br/>' . 'El valor de Y es: ' .  $y . '<br/>' . 'El resultado de la suma es: '  . $resuldato
+
+
+
+?>
